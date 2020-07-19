5197d8ed397acadf0a139b6bfbd57843448763f4
csabafarkas <csaba.farkas@wasptech.com>
2020-07-19 11:12:10 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
 router.get("/", (req, res) => {
@@ -12,6 +13,28 @@ router.get("/", (req, res) => {
   res.end();
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+app.get("/config", async (req, res) => {
+  const productsAll = await stripe.products.list();
+  const pricesAll = await stripe.prices.list();
+
+  const productsWithPrices = productsAll.data.map((p, i) => {
+    const price = pricesAll.data.find((pr) => pr.product === p.id);
+    return {
+      unitAmount: price.unit_amount,
+      currency: price.currency,
+      productName: p.name,
+      productDescription: p.description,
+      id: price.id,
+      images: p.images,
+    };
+  });
+
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    products: productsWithPrices,
+  });
+});
+
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());

4d5d7ae9678b0055c52362ba749b7da3ee8b71a6
csabafarkas <csaba.farkas@wasptech.com>
2020-07-19 11:06:40 +0100

    Update config.js

--- express/config.js
+++ express/config.js
@@ -7,6 +7,17 @@ const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
 // router.get("/", (req, res) => {
 //   res.writeHead(200, { "Content-Type": "text/html" });
 //   res.write("<h1>Hello from Express.js!</h1>");
@@ -37,17 +48,7 @@ app.get("/", async (req, res) => {
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
-app.use(
-  express.json({
-    // We need the raw body to verify webhook signatures.
-    // Let's compute it only when hitting the Stripe webhook endpoint.
-    verify: function (req, res, buf) {
-      if (req.originalUrl.startsWith("/webhook")) {
-        req.rawBody = buf.toString();
-      }
-    },
-  })
-);
+
 app.use(bodyParser.json());
 app.use("/.netlify/functions/config", router); // path must route to lambda
 app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));

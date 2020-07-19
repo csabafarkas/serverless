a2c68a544d03e042ec0d34979d7486456818c221
csabafarkas <csaba.farkas@wasptech.com>
2020-07-19 11:16:37 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -35,6 +35,44 @@ router.get("/config", async (req, res) => {
   });
 });
 
+router.get("/checkout-session", async (req, res) => {
+  const { sessionId } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  res.send(session);
+});
+
+router.post("/create-checkout-session", async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+  const { id, url, locale } = req.body;
+  // Create new Checkout Session for the order
+  // Other optional params include:
+  // [billing_address_collection] - to display billing address details on the page
+  // [customer] - if you have an existing Stripe Customer ID
+  // [customer_email] - lets you prefill the email input in the Checkout page
+  // For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: process.env.PAYMENT_METHODS.split(", "),
+    mode: "payment",
+    locale: locale,
+    line_items: [
+      {
+        price: id,
+        quantity: 1,
+      },
+    ],
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    success_url: `${
+      url ? url : domainURL
+    }/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${url ? url : domainURL}/canceled.html`,
+  });
+
+  res.send({
+    sessionId: session.id,
+  });
+});
+
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
